70544d4cb51c05118fcaf4f550efe08c2e4e1e5f
Muthu <muthu101kumaran@gmail.com>
2020-04-09 19:26:40 +0530

    heroku ready

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express=require('express');
-const port=process.env.port;
+const port= process.env.PORT ||8000;
 
 const app=express();
 const db=require('./config/mongoose');                      //connecting to db
